9841feff293eb37b4a91e1069e9649653e704aec
Samuel Mullen <samuel@samuelmullen.com>
2017-05-26 13:14:06 -0500

    Add earmark and pdf_generator modules

--- mix.exs
+++ mix.exs
@@ -15,7 +15,10 @@ defmodule EZPDF.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [
+      :logger, 
+      :pdf_generator
+    ]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +31,9 @@ defmodule EZPDF.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :earmark, "~> 1.2" },
+      { :pdf_generator, "~> 0.3" }
+    ]
   end
 end
